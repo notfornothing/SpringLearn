0c6dabd43808e42a154bfa3534ca779f58f8a853
notfornothing <798953052@qq.com>
2025-03-16 18:42:50 +0800

    bean 的创建方式 factoryBean

---
+++ src/main/java/com/example/spring/factorybean/MyBeanFactoryBean.java
@@ -0,0 +1,24 @@
+package com.example.spring.factorybean;
+
+import org.springframework.beans.factory.FactoryBean;
+
+public class MyBeanFactoryBean implements FactoryBean<MyBean> {
+
+    @Override
+    public MyBean getObject() throws Exception {
+        // 自定义创建 MyBean 的逻辑
+        MyBean myBean = new MyBean();
+        myBean.setMessage("通过 FactoryBean 创建的 MyBean");
+        return myBean;
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return MyBean.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return true; // 单例模式
+    }
+} 
