9e089e4339dbb0d29d12e2de94a7b4eaeadf5ba8
notfornothing <798953052@qq.com>
2025-03-17 14:52:58 +0800

    di

---
+++ src/main/java/cn/leijiba/spring/di/AnotherService.java
@@ -0,0 +1,10 @@
+package cn.leijiba.spring.di;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnotherService {
+    public void assist() {
+        System.out.println("AnotherService is assisting.");
+    }
+} 
--- src/main/java/cn/leijiba/spring/di/SetterInjection.java
+++ src/main/java/cn/leijiba/spring/di/SetterInjection.java
@@ -1,19 +1,28 @@
 package cn.leijiba.spring.di;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SetterInjection {
     private String message;
+    private AnotherService anotherService;
 
-    // Setter 方法注入
+    // @Value Setter 方法注入
     @Value("Hello from SetterInjection")
     public void setMessage(String message) {
         this.message = message;
     }
 
+    // @Autowired Setter 方法注入
+    @Autowired
+    public void setAnotherService(AnotherService anotherService) {
+        this.anotherService = anotherService;
+    }
+
     public void help() {
         System.out.println("SetterInjection is helping with message: " + message);
+        anotherService.assist();
     }
 } 
